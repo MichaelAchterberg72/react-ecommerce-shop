7fd35e7da4c3c9a8fb38152fda57d1d7614f4b51
Michael Achterberg <machterberg@devoptec.com>
2022-12-31 11:12:47 +0200

    add reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect, useReducer } from "react";
 
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 
 export const UserContext = createContext({
@@ -37,7 +38,7 @@ export const UserProvider = ({ children }) => {
     const { currentUser } = state;
 
     const setCurrentUser = (user) => {
-        dispatch({ type:USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
     };
     
     const value = { currentUser, setCurrentUser };
